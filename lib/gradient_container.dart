4ce48452bb1a5a97008d6e1b40e644b22b132e4b
thephotogenicbug <navinkumarachari@gmail.com>
2023-08-06 18:52:33 +0530

    Generate random number and change the image when user clicker on roll dice button

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,10 +1,9 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
-
-  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -19,25 +18,8 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/dice-2.png',
-              width: 200,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const Text("Roll Dice"),
-            )
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
